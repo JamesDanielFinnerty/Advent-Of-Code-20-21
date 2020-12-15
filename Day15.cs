086cb343917cfc87a8cc10fbf1e95f7e52e2e84e
JamesDanielFinnerty <75752549+JamesDanielFinnerty@users.noreply.github.com>
2020-12-15 15:22:05 +0000

    Tweaks and comments

--- Day15.cs
+++ Day15.cs
@@ -7,30 +7,40 @@ namespace AdventOfCode
 {
     public static class Day15
     {
-        public static int Part1(string input, long targetTurn)
+        public static int Solve(string input, long targetTurn)
         {
             var numbers = Array.ConvertAll(
                                 input.Split(','),
                                 x => int.Parse(x)
                              );
 
-            var turns = new int[30000000];
+            // create array to cover all turns we need to simulate
+            var turns = new int[targetTurn];
+
+            // use -1 as a null val.
             Array.Fill(turns, -1);
 
             var turn = 1;
 
+            // load given starter turns into our array
             for (; turn < numbers.Length + 1; turn++)
             {
                 turns[numbers[turn - 1]] = turn;
             }
 
+            // first spoken number after starters will always be 0.
             var currentNumber = 0;
 
             for (; turn < targetTurn; turn++)
             {
+                // get and store the previous time our current number was spoken
                 var previousTurn = turns[currentNumber];
+                // overwrite in the area with this turn
                 turns[currentNumber] = turn;
 
+                // if previous time spoken was not null, then set
+                // current number as the differance
+                // else speak 0
                 if(previousTurn != -1)
                 {
                     currentNumber = turn - previousTurn;
